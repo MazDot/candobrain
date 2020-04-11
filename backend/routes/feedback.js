c05ffe41699c2bb1c638fb5d4ab587661a9a4e48
Hamed Mazaheri <hamedmaz400@gmail.com>
2020-04-11 15:53:13 +0300

    onemorer

--- backend/routes/feedback.js
+++ backend/routes/feedback.js
@@ -1,12 +1,6 @@
 const router = require('express').Router();
 let Feedback = require('../models/feedbacks.models');
 
-router.route('/').get((req, res) => {
-    Feedback.find()
-      .then(feedbacks => res.json(feedbacks))
-      .catch(err => res.status(400).json('Error: ' + err));
-  });
-
 router.route('/add').post((req, res) => {
   const name = req.body.name;
   const message = req.body.message;
