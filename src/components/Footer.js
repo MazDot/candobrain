b5a6a005934c9a355828b1aa24513cddfc3d9387
Hamed Mazaheri <hamedmaz400@gmail.com>
2020-04-12 11:48:07 +0300

    whole change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import axios from 'axios';
 import instaIcon from '../assets/instaIcon.png';
 import youtubeIcon from '../assets/youtubeIcon.png';
 import Row from 'react-bootstrap/Row';
@@ -8,73 +7,6 @@ import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 
 class Footer extends React.Component {
-  constructor (props){
-    super (props);
-
-    this.onChangeName = this.onChangeName.bind(this);
-    this.onChangeMessage = this.onChangeMessage.bind(this);
-    this.onChangeAge = this.onChangeAge.bind(this);
-    this.onChangeEmail = this.onChangeEmail.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-
-    this.state = {
-      name: '',
-      message: '',
-      age: 0,
-      email: '',
-
-    }
-  }
-  onChangeName (e) {
-    this.setState ({
-      name: e.target.value
-    });
-
-  }
-  onChangeMessage (e) {
-    this.setState ({
-      message: e.target.value
-    });
-
-  }
-  onChangeAge (e) {
-    this.setState ({
-      age: e.target.value
-    });
-
-  }
-  onChangeEmail (e) {
-    this.setState ({
-      email: e.target.value
-    });
-
-  }
-  onSubmit (e) {
-    e.preventDefault();
-    const feedback = {
-      name: this.state.name,
-      message: this.state.message,
-      email: this.state.email
-
-    }
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(feedback.email))
-    {
-      axios.post('https://candobrain.herokuapp.com/feedback/add', feedback)
-      .then(res => console.log (res.data));
-      //maybe alert them its sent
-      alert('Thanks for the feedback');
-      //same page but clear inputs in forms
-      this.setState ({
-        name: '',
-        message: '',
-        email: ''
-      });
-    } else {
-      alert("You have entered an invalid email address!")
-    }
-    
-  }
-
   render () {
   return (
     <FooterContainer className="main-footer">
@@ -123,42 +55,8 @@ class Footer extends React.Component {
               </ul>
             </div>
             <div className="col-md-6 col-sm-6">
-              {/* Feedback Part */}
-              <form onSubmit = {this.onSubmit} >
-                <div className = "form-group">
-                  <label>Name: </label>
-                  <input type ="text"
-                  required
-                  className ="form-control"
-                  value = {this.state.name}
-                  onChange = {this.onChangeName}
-                  />
-                </div>
-                <div className = "form-group">
-                  <label>Email: </label>
-                  <input type ="text"
-                  required
-                  className ="form-control"
-                  value = {this.state.email}
-                  onChange = {this.onChangeEmail}
-                  />
-                </div>
-                <div className = "form-group">
-                  <label>Message: </label>
-                  <input type ="text"
-                  required
-                  className ="form-control"
-                  value = {this.state.message}
-                  onChange = {this.onChangeMessage}
-                  />
-                </div>
-                <div className = "form-group">
-                  <input type="submit" value="Send" className = "btn btn-primary"></input>
-                </div>
-              </form>
             </div>
           </div>
-          {/* Footer Bottom */}
           <div className="footer-bottom">
             <p className="text-xs-center">
               &copy;{new Date().getFullYear()} All Rights
